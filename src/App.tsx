4eceffa07771169d5d30dd01542ccdd4c498fcee
Kate Howard <kate@simpleweb.co.uk>
2020-03-24 16:55:26 +0000

    Start and stop timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import useInterval from './useInterval';
 
 function App() {
   const textToCopy: string = "Lorem ipsum";
   const [textTyped, setTextTyped] = useState<string>("");
+  const [timing, toggleTiming] = useState<boolean>(false);
+  const [time, setTime] = useState<number>(0);
+
+  useInterval(() => {
+    setTime(time + 1);
+  }, timing ? 1000 : null);
+
+  useEffect(() => {
+    if (textTyped === textToCopy) {
+      toggleTiming(false);
+    }
+  }, [textTyped]);
 
   return (
-    <>
+    <div className="container">
       <p>{textToCopy}</p>
       <textarea onChange={e => setTextTyped(e.target.value)}></textarea>
       <div style={{ color: textTyped === textToCopy ? 'green' : 'black'}}>{textTyped}</div>
-    </>
+      <p>{time}</p>
+      <button onClick={() => toggleTiming(true)}>Start</button>
+    </div>
   );
 }
 
