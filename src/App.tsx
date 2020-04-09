744535b3e4d998082503bc3df39a7f1d3116db80
Kate Howard <k.l.howard.1992@gmail.com>
2020-04-09 13:49:41 +0100

    Get text to begin with, start timer by typing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import useInterval from './useInterval';
 import { TextField } from './Components/TextField';
 
 function App() {
-  const endpoint = "http://www.randomtext.me/api/lorem/p-1/20-30";
+  const endpoint = "http://www.randomtext.me/api/lorem/p-1/40";
   const [textToCopy, setTextToCopy] = useState<string>("");
   const [textTyped, setTextTyped] = useState<string>("");
   const [timing, toggleTiming] = useState<boolean>(false);
@@ -15,15 +15,7 @@ function App() {
     if (time <= 1) toggleTiming(false);
   }, timing ? 1000 : null);
 
-  useEffect(() => {
-    if (textTyped === textToCopy) {
-      toggleTiming(false);
-    }
-  }, [textTyped, textToCopy]);
-
-
-  function start() {
-    toggleTiming(true);
+  function getText() {
     fetch(endpoint)
       .then(blob => blob.json())
       .then(data =>
@@ -31,14 +23,20 @@ function App() {
       );
   }
 
+  function type(e: any) {
+    if (textTyped === "") toggleTiming(true);
+    setTextTyped(e.target.value);
+  }
+
+  useEffect(getText, []);
+
   return (
     <div className="container">
       <p>{textToCopy.split(" ").map((word, index) => (
         <span key={index} style={{ color: index === textTyped.split(" ").length - 1 ? 'green' : textTyped.split(" ")[index] === word ? 'grey' : 'black'}}> {word}</span>
       ))}</p>
-      <TextField onChange={e => setTextTyped(e.target.value)} />
+      <TextField onChange={type} />
       <p>{Math.floor(time / 60)}:{time % 60 < 10 ? `0${time % 60}` : time % 60}</p>
-      <button onClick={start}>Start</button>
       <p>Words typed: {textTyped.split(" ").length - 1}</p>
       <p>Errors: {textTyped.split(" ").filter((word, index) => textToCopy.split(" ")[index] !== word).length}</p>
     </div>
