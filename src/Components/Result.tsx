bf632adb6ed969b2f04d8691d7d596ef785b1ab0
Kate Howard <k.l.howard.1992@gmail.com>
2020-04-12 15:19:32 +0100

    Display result on chart

--- src/Components/Result.tsx
+++ src/Components/Result.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 
 interface Props {
   words: number,
@@ -8,13 +9,21 @@ interface Props {
 
 export const Result: React.FC<Props> = ({ words, errors, restart }) => {
   const accuracy = (words - errors) / words;
+  const speed = Math.floor(words * accuracy);
+  const labels = [20,40,60,80];
 
   return (
     <div className="overlay">
       <div className="modal">
-        <p>{words} words typed</p>
-        <p>{Math.floor(accuracy * 100)}% accuracy</p>
-        <p>Adjusted speed: {Math.floor(words * accuracy)} WPM</p>
+        <p>{words} words typed <FaTimes /> {Math.floor(accuracy * 100)}% accuracy</p>
+        <h3>Adjusted speed: {speed} WPM</h3>
+        <div className="result-chart">
+          {labels.map(number => <div className="marker" style={{ left: `${number * 3}px` }} />)}
+          <div className="marker result-marker" style={{ left: `${speed * 3}px` }} />
+        </div>
+        <div className="chart-labels">
+          {labels.map(number => <span>{number}</span>)}
+        </div>
         <button onClick={restart}>Try again</button>
       </div>
     </div>
