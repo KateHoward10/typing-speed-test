f8a110660393bbed023b3abda4849aeaac1b8e31
Kate Howard <k.l.howard.1992@gmail.com>
2020-04-16 22:01:25 +0100

    Updates

---
+++ precache-manifest.37cd1e9b0149406e491820780c2811ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b702e5cdefdfaeba6f02ccf50d98f1",
+    "url": "/typing-speed-test/index.html"
+  },
+  {
+    "revision": "10d435986706f370dffb",
+    "url": "/typing-speed-test/static/css/main.38f61a2a.chunk.css"
+  },
+  {
+    "revision": "c7e31f0211253d434ea8",
+    "url": "/typing-speed-test/static/js/2.cfe207cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typing-speed-test/static/js/2.cfe207cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d435986706f370dffb",
+    "url": "/typing-speed-test/static/js/main.451af3c7.chunk.js"
+  },
+  {
+    "revision": "96c4e2c9019b93f23126",
+    "url": "/typing-speed-test/static/js/runtime-main.3fdfff73.js"
+  }
+]);
